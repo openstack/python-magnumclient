b3ef41d82e9755a8b16fcc817966d381e6ae2cf8
Fang Fenghua <449171342@qq.com>
2015-04-17 08:28:13 +0000

    Add bay status check when pod create.
    Now when we create pod,we didn't check the bay status
    it require.It may cause some strange error.
    
    Change-Id: Icfb1ccb317fe849f333b0b25666a27ce80d16508
    Closes-bug:#1445319
    Closes-bug:#1442452

--- magnumclient/tests/v1/test_shell.py
+++ magnumclient/tests/v1/test_shell.py
@@ -178,6 +178,7 @@ class ShellTest(base.TestCase):
         client_mock = mock.MagicMock()
         bay = mock.MagicMock()
         bay.uuid = 'uuid'
+        bay.status = 'CREATE_COMPLETE'
         client_mock.bays.get.return_value = bay
 
         args = mock.MagicMock()
@@ -191,6 +192,24 @@ class ShellTest(base.TestCase):
         shell.do_pod_create(client_mock, args)
         client_mock.pods.create.assert_called_once_with(
             manifest_url=manifest_url, bay_uuid=bay.uuid)
+
+    def test_do_pod_create_with_bay_in_wrong_status(self):
+        client_mock = mock.MagicMock()
+        bay = mock.MagicMock()
+        bay.uuid = 'uuid'
+        bay.status = 'XXX'
+        client_mock.bays.get.return_value = bay
+
+        args = mock.MagicMock()
+        manifest_url = "test_url"
+        args.manifest_url = manifest_url
+        bay_id_or_name = "xxx"
+        args.bay = bay_id_or_name
+        manifest = "test_manifest"
+        args.manifest = manifest
+
+        shell.do_pod_create(client_mock, args)
+        self.assertFalse(client_mock.pods.create.called)
 
     def test_do_pod_delete(self):
         client_mock = mock.MagicMock()
--- magnumclient/v1/shell.py
+++ magnumclient/v1/shell.py
@@ -264,7 +264,11 @@ def do_pod_list(cs, args):
 def do_pod_create(cs, args):
     """Create a pod."""
     bay = cs.bays.get(args.bay)
-
+    if bay.status not in ['CREATE_COMPLETE', 'UPDATE_COMPLETE']:
+        print('Bay status for %s is: %s. We can not create a %s there'
+              'until the status is CREATE_COMPLETE or UPDATE_COMPELTE.' %
+              (bay.uuid, bay.status, "pod"))
+        return
     opts = {}
     opts['manifest_url'] = args.manifest_url
     opts['bay_uuid'] = bay.uuid
