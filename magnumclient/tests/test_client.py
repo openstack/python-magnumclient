e7982d263c98059cc09a75191a5b2bdefb8ea60c
Monty Taylor <mordred@inaugust.com>
2015-11-07 08:26:20 -0500

    Add unversioned client constructor
    It's a very common pattern in OpenStack to have a base level client
    file that has a client constructor that takes a version as the first
    parameter. This allows developers to write code that just imports the
    client, rather than relying on python import paths for version
    selection, which in turn allows for future version autodetection with
    things like microversions.
    
    Closes-Bug: #1514732
    
    Change-Id: Ibf8df9c8dbdfe27d9a387304e05afe95d3e33f49

---
+++ magnumclient/tests/test_client.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2015 IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+import testtools
+
+from magnumclient import client
+
+
+class ClientTest(testtools.TestCase):
+
+    @mock.patch('magnumclient.v1.client.Client')
+    def test_no_version_argument(self, mock_magnum_client):
+        client.Client(input_auth_token='mytoken', magnum_url='http://myurl/')
+        mock_magnum_client.assert_called_with(
+            input_auth_token='mytoken', magnum_url='http://myurl/')
+
+    @mock.patch('magnumclient.v1.client.Client')
+    def test_valid_version_argument(self, mock_magnum_client):
+        client.Client(version='1', magnum_url='http://myurl/')
+        mock_magnum_client.assert_called_with(magnum_url='http://myurl/')
+
+    @mock.patch('magnumclient.v1.client.Client')
+    def test_invalid_version_argument(self, mock_magnum_client):
+        self.assertRaises(
+            ValueError,
+            client.Client, version='2', magnum_url='http://myurl/')
