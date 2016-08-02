41563dc1620309149945565071de9d159ce58f9b
Rajiv Kumar <rajiv.kumar@nectechnologies.in>
2016-08-02 11:28:14 +0530

    Enabled magnum client to display detailed information
    Enabled magnum client to display following information
     - disabled
     - disabled_reason
     - created_at
     - updated_at
    
    disabled field is not returned by the magnum server, but magnum
    server code will be modified for, bug #1607228. Server will be
    returning this field after fix of mentioned bug.
    
    Change-Id: I5bab76bf1fa476394c287ddc781f43bca2e5f035
    Closes-bug: #1607234

--- magnumclient/v1/mservices_shell.py
+++ magnumclient/v1/mservices_shell.py
@@ -20,6 +20,8 @@ from magnumclient.common import utils as magnum_utils
 def do_service_list(cs, args):
     """Print a list of magnum services."""
     mservices = cs.mservices.list()
-    columns = ('id', 'host', 'binary', 'state')
+    columns = ('id', 'host', 'binary', 'state', 'disabled',
+               'disabled_reason', 'created_at', 'updated_at')
+
     utils.print_list(mservices, columns,
                      {'versions': magnum_utils.print_list_field('versions')})
