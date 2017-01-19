b68ce22fd13c4af291683b833b79be0affe07351
yatin <yatin.karel@nectechnologies.in>
2017-01-19 11:14:25 +0530

    Fix: swarm cluster-config, bay-config with tls_disabled
    magnum cluster-config, bay-config for swarm cluster
    didn't work with tls_disabled.
    
    With this patch following is done:-
    if tls_disabled then set DOCKER_TLS_VERIFY=""
    if not tls_disabled then set DOCKER_TLS_VERIFY=True
    
    Change-Id: I57eb1d49b5157958f9c5f6d89a613896300fdb6e
    Closes-Bug: #1647254

--- magnumclient/v1/bays_shell.py
+++ magnumclient/v1/bays_shell.py
@@ -328,6 +328,9 @@ def _config_bay_swarm(bay, baymodel, cfg_dir, force=False):
                   "setenv DOCKER_TLS_VERIFY %(tls)s\n"
                   % {'docker_host': bay.api_address,
                      'cfg_dir': cfg_dir,
+                     'tls': ""} if baymodel.tls_disabled else
+                    {'docker_host': bay.api_address,
+                     'cfg_dir': cfg_dir,
                      'tls': not baymodel.tls_disabled}
                   )
     else:
@@ -335,6 +338,9 @@ def _config_bay_swarm(bay, baymodel, cfg_dir, force=False):
                   "export DOCKER_CERT_PATH=%(cfg_dir)s\n"
                   "export DOCKER_TLS_VERIFY=%(tls)s\n"
                   % {'docker_host': bay.api_address,
+                     'cfg_dir': cfg_dir,
+                     'tls': ""} if baymodel.tls_disabled else
+                    {'docker_host': bay.api_address,
                      'cfg_dir': cfg_dir,
                      'tls': not baymodel.tls_disabled}
                   )
--- magnumclient/v1/clusters_shell.py
+++ magnumclient/v1/clusters_shell.py
@@ -333,6 +333,9 @@ def _config_cluster_swarm(cluster, cluster_template, cfg_dir, force=False):
                   "setenv DOCKER_TLS_VERIFY %(tls)s\n"
                   % {'docker_host': cluster.api_address,
                      'cfg_dir': cfg_dir,
+                     'tls': ""} if cluster_template.tls_disabled else
+                    {'docker_host': cluster.api_address,
+                     'cfg_dir': cfg_dir,
                      'tls': not cluster_template.tls_disabled}
                   )
     else:
@@ -340,6 +343,9 @@ def _config_cluster_swarm(cluster, cluster_template, cfg_dir, force=False):
                   "export DOCKER_CERT_PATH=%(cfg_dir)s\n"
                   "export DOCKER_TLS_VERIFY=%(tls)s\n"
                   % {'docker_host': cluster.api_address,
+                     'cfg_dir': cfg_dir,
+                     'tls': ""} if cluster_template.tls_disabled else
+                    {'docker_host': cluster.api_address,
                      'cfg_dir': cfg_dir,
                      'tls': not cluster_template.tls_disabled}
                   )
