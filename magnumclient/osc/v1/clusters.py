6b756aa75fac1e30df177adf3e0ab65898b7df06
Feilong Wang <flwang@catalyst.net.nz>
2019-03-26 06:01:54 +1300

    Support upgrade API
    Task: 30168
    Story: 2002210
    
    Change-Id: I458985f97e1e9bbe89702ce04e74bdcf66e36244

--- magnumclient/osc/v1/clusters.py
+++ magnumclient/osc/v1/clusters.py
@@ -413,3 +413,45 @@ class ResizeCluster(command.Command):
                                    parsed_args.nodegroup)
         print("Request to resize cluster %s has been accepted." %
               parsed_args.cluster)
+
+
+class UpgradeCluster(command.Command):
+    _description = _("Upgrade a Cluster")
+
+    def get_parser(self, prog_name):
+        parser = super(UpgradeCluster, self).get_parser(prog_name)
+        parser.add_argument(
+            'cluster',
+            metavar='<cluster>',
+            help=_('The name or UUID of cluster to update'))
+
+        parser.add_argument(
+            'cluster_template',
+            help=_("The new cluster template ID will be upgraded to."))
+
+        parser.add_argument(
+            '--max-batch-size',
+            metavar='<max_batch_size>',
+            type=int,
+            default=1,
+            help=_("The max batch size for upgrading each time."))
+
+        parser.add_argument(
+            '--nodegroup',
+            metavar='<nodegroup>',
+            help=_('The name or UUID of the nodegroup of current cluster.'))
+
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)", parsed_args)
+
+        mag_client = self.app.client_manager.container_infra
+        cluster = mag_client.clusters.get(parsed_args.cluster)
+
+        mag_client.clusters.upgrade(cluster.uuid,
+                                    parsed_args.cluster_template,
+                                    parsed_args.max_batch_size,
+                                    parsed_args.nodegroup)
+        print("Request to upgrade cluster %s has been accepted." %
+              parsed_args.cluster)
